379e7b667825673287b34a4bad7a54a4823d2ed4
Lemuel Beduya <lemuel.beduya@orangeandbronze.com>
2025-07-15 10:51:34 +0800

    Removed unnecessary prerender setting

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
-
 export function load() {
     redirect(307, '/todo');
 }
-
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
 
 
