e51c7effc294343383fa3aff457b20dbedd955a8
Lemuel Beduya <lemuel.beduya@orangeandbronze.com>
2025-07-18 17:46:44 +0800

    Removed custom text color on custom error page

--- src/routes/todo/+error.svelte
+++ src/routes/todo/+error.svelte
@@ -1,7 +1,7 @@
 <div class="container mx-auto p-4">
   <h1 class="text-2xl font-bold mb-4">Error</h1>
-  <p class="text-gray-600 mb-4">An error occurred while acessing the page.</p>
-  <p class="text-gray-600 mb-4">
+  <p class="mb-4">An error occurred while acessing the page.</p>
+  <p class="mb-4">
     Please check the URL or return to the <a
       href="/"
       class="text-blue-500 hover:underline">home page</a
